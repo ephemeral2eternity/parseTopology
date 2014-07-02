b08879bafcbac01e4c621174ed1e4b96aebdf9a7
ephemeral2eternity <chenw@andrew.cmu.edu>
2014-07-02 15:46:48 +0100

    updated genDeployment

--- genDeployment/genDeployment.py
+++ genDeployment/genDeployment.py
@@ -6,19 +6,28 @@ from ElementTree_pretty import prettify
 totalArgs = len(sys.argv)
 cmdArgs = sys.argv
 
-if (totalArgs < 2):
-        print "Usage : python genDeployment.py hosts.csv deployment.xml\n   hosts.csv : the host name file you want to read host names.\n    deployment.xml : the deployment file name you want to generate\n"
+if (totalArgs < 3):
+        print "Usage : python genDeployment.py server.csv clients.csv deployment.xml\n   hosts.csv : the host name file you want to read host names.\n    deployment.xml : the deployment file name you want to generate\n"
         sys.exit()
 
-hostFileName = cmdArgs[1]
-deploymentFileName = cmdArgs[2]
+serverFileName = cmdArgs[1]
+clientFileName = cmdArgs[2]
+deploymentFileName = cmdArgs[3]
 
 try:
-	with open(hostFileName, 'rb') as hostsFile:
-		hosts = hostsFile.read().split("\n")
-		print hosts
+	with open(serverFileName, 'rb') as serversFile:
+		servers = serversFile.read().split("\n")
+		print servers
 except IOError:
-	print "Could not read file ", hostFileName, ", please check the availability of the file!"
+	print "Could not read file ", serverFileName, ", please check the availability of the file!"
+	sys.exit()
+
+try:
+	with open(clientFileName, 'rb') as clientsFile:
+		clients = clientsFile.read().split("\n")
+		print clients
+except IOError:
+	print "Could not read file ", clientFileName, ", please check the availability of the file!"
 	sys.exit()
 
 deploymentFile = open(deploymentFileName, 'w')
@@ -30,18 +39,19 @@ while '' in hosts:
 comment = ET.Comment('DOCTYPE deployment SYSTEM "https://github.com/ephemeral2eternity/simgrid_simulations.git"')
 platform = ET.Element('platform')
 platform.set('version', "3")
-for host in hosts:
+for server in servers:
 	curProc = ET.SubElement(platform, 'process')
 	curProc.set('function', "agentMngt.cacheAgent")
 	curProc.set('host', host)
-	# splHosts = random.sample(hosts, 10)
-	# while host in splHosts:
-	# 	splHosts = random.sample(hosts, 10)
 	argu = ET.SubElement(curProc, 'argument')
-	argu.set('value', hostFileName)
-	# for peer in splHosts:
-	# 	curArgu = ET.SubElement(curProc, 'argument')
-	#	curArgu.set('value', peer)
+	argu.set('value', serverFileName)
+
+for client in clients:
+	curProc = ET.SubElement(platform, 'process')
+	curProc.set('function', "agentMngt.clientAgent")
+	curProc.set('host', host)
+	argu = ET.SubElement(curProc, 'argument')
+	argu.set('value', serverFileName)
 
 # print ET.tostring(comment)
 # print prettify(platform)
